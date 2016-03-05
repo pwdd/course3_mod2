34c84edee2d0dbc93d4b3525ea97bb3e2fcfb491
pwdd <ainapinto@gmail.com>
2016-03-04 22:59:40 -0800

    collection 02

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,17 @@
 class Place
+  Mongo::Logger.logger.level = ::Logger::INFO
+
   def self.mongo_client
     Mongoid::Clients.default
   end
 
   def self.collection
     mongo_client[:places]
+  end
+
+  def self.load_all(file)
+    read = File.read(file)
+    hash = JSON.parse(read)
+    collection.insert_many(hash)
   end
 end
