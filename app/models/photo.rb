69e81694ec7228531a7463c8fb0eee49e9b0fc8a
pwdd <ainapinto@gmail.com>
2016-03-05 02:26:43 -0800

    photos 03

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,21 @@
 class Photo
+  attr_accessor :id, :location, :contents
+
+  def initialize(params)
+    @id = params[:_id].nil? ? params[:id] : params[:_id].to_s
+    if params[:metadata] && params[:metadata][:location]
+      if params[:metadata][:location].is_a?(Point)
+        @location = params[:metadata][:location]
+      else
+        @location = Point.new(params[:metadata][:location])
+      end
+    end
+
+    if params[:metadata] && params[:metadata][:place]
+      @place = params[:metadata][:place]
+    end
+  end
+
   def self.mongo_client
     Mongoid::Clients.default
   end
