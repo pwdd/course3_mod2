109df6a3252ab22fd2d7ae22ad381fd177f3e4db
pwdd <ainapinto@gmail.com>
2016-03-05 02:42:34 -0800

    photos 05

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo
   attr_accessor :id, :location, :contents
 
-  def initialize(params)
+  def initialize(params={})
     @id = params[:_id].nil? ? params[:id] : params[:_id].to_s
     if params[:metadata] && params[:metadata][:location]
       if params[:metadata][:location].is_a?(Point)
@@ -18,5 +18,31 @@ class Photo
 
   def self.mongo_client
     Mongoid::Clients.default
+  end
+
+  def persisted?
+    !@id.nil?
+  end
+
+  def save
+    description = {}
+
+    if persisted?
+      description[:metadata] = { location: @location.to_hash }
+      description[:metadata][:place] = @place
+      self.class.mongo_client.database.fs
+        .find(:_id=> BSON::ObjectId.from_string(@id))
+        .update_one(description)
+    else
+      geolocation = EXIFR::JPEG.new(@contents).gps
+      @location = Point.new({ lng: geolocation.longitude, lat: geolocation.latitude })
+      description[:content_type] = 'image/jpeg'
+      description[:metadata] = { location: @location.to_hash }
+      description[:metadata][:place] = @place
+      @contents.rewind
+      gridfs = Mongo::Grid::File.new(@contents.read, description)
+      id = self.class.mongo_client.database.fs.insert_one(gridfs)
+      @id = id.to_s
+    end
   end
 end
