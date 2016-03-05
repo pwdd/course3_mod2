e6c6dedccf4b09e0c29a9b7fac209521f0ecc68d
pwdd <ainapinto@gmail.com>
2016-03-05 00:59:41 -0800

    geo 01

--- app/models/place.rb
+++ app/models/place.rb
@@ -83,6 +83,14 @@ class Place
     collection.find.aggregate(q).map { |doc| doc[:_id].to_s }
   end
 
+  def self.create_indexes
+    collection.indexes.create_one(:'geometry.geolocation' => Mongo::Index::GEO2DSPHERE)
+  end
+
+  def self.remove_indexes
+    collection.indexes.drop_one('geometry.geolocation_2dsphere')
+  end
+
   def destroy
     self.class.collection.find(_id: BSON::ObjectId.from_string(@id)).delete_one
   end
